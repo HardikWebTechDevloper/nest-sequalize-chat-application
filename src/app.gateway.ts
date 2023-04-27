4f1c83c927b2e003316e8b37b22f5e2c2d7e2f70
hirendudhat <hiren@vibrant-info.com>
2023-04-27 17:11:06 +0530

    Socket API changes

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -23,20 +23,49 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
   }
 
   @SubscribeMessage('joined')
-  handleJoinedEvent(client: Socket, payload: { user: string }) {
-    const { user } = payload;
-    const socketId = client.id;
-    console.log(`${user} has joined `);
-    this.users[socketId] = user; // Store user information in the users property
-    client.broadcast.emit('userJoined', { user: 'Admin', message: `${user} has joined` });
-    client.emit('welcome', { user: 'Admin', message: `Welcome to the chat, ${user}` });
+  handleJoinedEvent(client: Socket, payload: { chatUser: string }) {
+    const { chatUser } = payload;
+    this.users[client.id] = chatUser;
+    console.log(`${chatUser} has joined `);
+    client.broadcast.emit('userJoined', { chatUser: "Admin", message: ` ${this.users[client.id]} has joined` });
+    client.emit('welcome', { chatUser: "Admin", message: `Welcome to the chat,${this.users[client.id]} ` })
   }
 
+  // @SubscribeMessage('joined')
+  // handleJoinedEvent(client: Socket, payload: { user: string }) {
+  //   const { user } = payload;
+  //   const socketId = client.id;
+  //   console.log(`${user} has joined `);
+  //   this.users[socketId] = user; // Store user information in the users property
+  //   client.broadcast.emit('userJoined', { user: 'Admin', message: `${user} has joined` });
+  //   client.emit('welcome', { user: 'Admin', message: `Welcome to the chat, ${user}` });
+  // }
+
+  // @SubscribeMessage('message')
+  // handleMessageEvent(client: Socket, payload: { message: string, id: string, name: string, groupId: number }) {
+  //   const { message, id, name, groupId } = payload;
+  //   const user = this.users[id];
+  //   this.server.emit('sendMessage', { user, message, id, name, groupId });
+  // }
+
   @SubscribeMessage('message')
-  handleMessageEvent(client: Socket, payload: { message: string, id: string }) {
-    const { message, id } = payload;
+  handleMessage(client: Socket, payload: { message: string, id: string, name: string, groupId: string }): void {
+    const { message, id, name, groupId } = payload;
+    const chatUser = this.users[id];
+    this.server.emit('sendMessage', { chatUser, message, id, name, groupId });
+  }
+
+  @SubscribeMessage('singleMessage')
+  handleSingleMessageEvent(client: Socket, payload: { message: string, id: string, name: string, userId: number }) {
+    const { message, id, name, userId } = payload;
     const user = this.users[id];
-    this.server.emit('sendMessage', { user, message, id });
+    this.server.emit('sendMessage', { user, message, id, name, userId });
+  }
+
+  @SubscribeMessage('request')
+  handleRequest(client: Socket, data: any, username: string): void {
+    const response = { data, username };
+    this.server.emit('response', response);
   }
 
   @SubscribeMessage('disconnecting')
